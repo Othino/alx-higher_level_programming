23299887993ca32cd6dfeb21be33b805f5c30aba
Othino <micahothino44@gmail.com>
2022-10-12 14:36:42 -0700

    a class Square that defines a square by: (based on 4-square.py)

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,84 +1,49 @@
 #!/usr/bin/python3
+"""
+This is the "Square"  module.
+This module provides a simple Square class with initialize size.
+Defaults size to 0. Raise error on invalid size inputs.
+Methods Getter and Setter properties for size.
+Method area returns size of area of the square.
+Methods __lt__, __le__, __eq__, __ne__, __gt__, __ge__ allow
+comparisons between Square objects and their sizes.
+"""
 
 
 class Square:
-    """
-    class square that has attributes:
-        size
-    some attributes are protected from input.
-    """
+    """A class that defines a square by size and can compute area"""
     def __init__(self, size=0):
-        """
-        initialization function for our square clasee
-        """
-        if self.__validate_size(size):
-            self.__size = size
-
-    def __eq__(self, other):
-        """
-        used by == to check equality
-        """
-        return (self.area() == other.area())
-
-    def __ne__(self, other):
-        """
-        used by != to check equality
-        """
-        return (self.area() != other.area())
-
-    def __lt__(self, other):
-        """
-        used by < to check equality
-        """
-        return (self.area() < other.area())
-
-    def __le__(self, other):
-        """
-        used by <= to check equality
-        """
-        return (self.area() <= other.area())
-
-    def __gt__(self, other):
-        """
-        used by > to check equality
-        """
-        return (self.area() > other.area())
-
-    def __ge__(self, other):
-        """
-        used by >= to check equality
-        """
-        return (self.area() >= other.area())
+        self.size = size
 
     @property
     def size(self):
-        """
-        getter for size attribute
-        """
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """
-        setter for size attribute
-        """
-        if self.__validate_size(value):
-            self.__size = value
-
-    def area(self):
-        """
-        calculates the area of the square
-        """
-        return self.__size ** 2
-
-    def __validate_size(self, size):
-        """
-        validates the size, checking for errors
-        """
+    def size(self, size):
         if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            return True
-        return False
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __lt__(self, other):
+        return self.__size < other.size
+
+    def __le__(self, other):
+        return self.__size <= other.size
+
+    def __eq__(self, other):
+        return self.__size == other.size
+
+    def __ne__(self, other):
+        return self.__size != other.size
+
+    def __gt__(self, other):
+        return self.__size > other.size
+
+    def __ge__(self, other):
+        return self.__size >= other.size
