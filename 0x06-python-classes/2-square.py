0ad16da577d59f86a47447707fea6b29412e9b14
Othino <micahothino44@gmail.com>
2022-10-12 10:17:55 -0700

    a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,10 +7,24 @@ Size defaults to 0. Raise errors on invalid inputs.
 """
 
 class Square:
+    
     """A class that defines a squuare by size"""
+
     def __init__(self, size=0):
+
+        """The __init__ method for Square class
+        
+        Args:
+           size: (:obj: 'int', optional): A private instance size
+
+        Raises:
+           TypeError: Exception if size is not an integer
+           ValueError: Exception if size is less that 0
+        """
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
