cc5eb467516eeb59d22bd2e6bf8aead3f45f3bd8
Othino <micahothino44@gmail.com>
2022-10-18 11:15:49 -0700

    a function magic_string() that returns a string “BestSchool” n times the number of the iteration

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+Created: October 2022
+@author: Micah
+"""
+
 def magic_string(l=[]):
+    """A function magic_string() that returns a string “BestSchool” n times the number of the iteration"""
     l += ["BestSchool"]
     return (", ".join(l))
