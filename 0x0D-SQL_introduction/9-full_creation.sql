b3a281107b77188ca971a9d882aea21d3c2e79c5
Othino <micahothino44@gmail.com>
2022-11-29 13:41:55 -0800

    Creating new table and adding multiple rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,9 +2,7 @@
 -- DDL query to create a new table
 
 CREATE TABLE IF NOT EXISTS second_table
-(id INT,
-    name VARCHAR(256),
-    score INT);
+(id INT, name VARCHAR(256), score INT);
 
 -- DML query to insert first row into the table
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
