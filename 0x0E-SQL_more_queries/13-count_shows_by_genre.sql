709d39d9e5d3915d58fa89e9bb59dba3e40548ef
Othino <micahothino44@gmail.com>
2022-11-30 12:30:41 -0800

    Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,23 +1,8 @@
 -- List tv shows and their genres
--- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT tv_genres.name AS genre,
 COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_show_genres
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY genre
-ORDER BY number_of_shows DESC;
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-S-- List tv shows and their genres
--- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
-SELECT tv_genres.name AS genre,
-COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM tv_show_genres
-INNER JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
