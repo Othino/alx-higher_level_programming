73906bd43032137789f2013ef2bb0ae27551aa19
Othino <micahothino44@gmail.com>
2022-12-20 03:39:01 -0800

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const argc  process.argv.length;
+const argc = process.argv.length;
 if (argc > 3) {
     console.log('Arguments found');
-} else if (argc === 3) {
-    console.log('Arguments found');
-} else {
-    console.log('No rgument');
+}
+else if (argc === 3) {
+    console.log('Argument found');
+}
+else {
+    console.log('No argument');
 }
