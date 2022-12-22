07f51579df89c250f14d7b8754a51a7d85dcd382
Othino <micahothino44@gmail.com>
2022-12-22 06:15:04 -0800

    Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-Lists all states from the database hbtn_0e_0_usa
+Lists all states with a name starting with N
 """
 
 import MySQLdb
@@ -18,7 +18,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', user=username,
                          passwd=password, db=data, port=3306)
     cur = db.cursor()
-    num_rows = cur.execute('SELECT * FROM states ORDER BY states.id;')
+    num_rows = cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY\
+                           'N%' ORDER BY states.id;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-Lists all states from the database hbtn_0e_0_usa
+List all cities from a database
 """
 
 import MySQLdb
@@ -18,7 +18,10 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', user=username,
                          passwd=password, db=data, port=3306)
     cur = db.cursor()
-    num_rows = cur.execute('SELECT * FROM states ORDER BY states.id;')
+    num_rows = cur.execute("SELECT cities.id, cities.name, states.name\
+                           FROM cities INNER JOIN states\
+                           ON cities.state_id=states.id\
+                           ORDER BY cities.id;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
