ffeb7b506046ae49a48c2c7dc9d27b964a604be2
Othino <micahothino44@gmail.com>
2023-01-16 14:44:07 -0800

    Network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the bodyof the response
-curl -s $1 --include | grep -i Content-Length | awk '{print $2}'
+curl -ls "$1" | grep Content-Length | cut -f2 -d' '
